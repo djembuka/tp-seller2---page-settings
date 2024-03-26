9e35b5f173eaa9afb21d704d2656c26aae59b64a
djembuka <djembuka@gmail.com>
2024-03-26 17:26:06 +0300

    Checkbox fixed

--- src/components/controls/ControlCheckboxCheckbox.vue
+++ src/components/controls/ControlCheckboxCheckbox.vue
@@ -43,7 +43,7 @@ export default {
         return this.control.checked;
       },
       set(checked) {
-        this.$emit('input', checked);
+        this.$emit('input', { checked });
       },
     },
     invalid() {
